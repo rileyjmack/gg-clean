3d57cc524a38aa6109c66a6a36921b8bf45a7967
Riley Mack <riley.j.mack@gmail.com>
2023-04-04 15:31:49 -0400

    testing post

--- src/components/FormSubmission.js
+++ src/components/FormSubmission.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Services from "./Services";
 
 const FormSubmission = () => {
   return (
     <div>
       <h1>Thank You For Your Request!</h1>
-      <h2> Our Team Will Contact You Shortly Either Via Email or Phone</h2>
+      <h2>Our Team Will Contact You Shortly Either Via Email or Phone</h2>
+      <Link to={"/services"}>
+        <div>HAN</div>
+      </Link>
     </div>
   );
 };

51ea67f418b21f1cda839e858c9d07f4bed673a2
Riley Mack <riley.j.mack@gmail.com>
2023-04-10 15:05:36 -0400

    fixed modal

--- src/components/Navb.js
+++ src/components/Navb.js
@@ -26,12 +26,7 @@ const Navb = () => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </p>
+          <h4>Call Us Anytime Between the Hours of 8:00am-5:00pm</h4>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>

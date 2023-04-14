4c6ae914d26dbad4cf17a607aaf23bbda2d46e8e
Riley Mack <riley.j.mack@gmail.com>
2023-04-14 11:27:22 -0400

    vercel express test

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,16 @@
-const router = require("express").Router();
-module.exports = router;
+const app = require("express")();
+const { v4 } = require("uuid");
 
-router.use("/clients", require("./clients"));
-
-router.use((req, res, next) => {
-  const error = new Error("Not Found");
-  error.status = 404;
-  next(error);
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;

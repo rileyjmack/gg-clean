61fc61c1ccc78c09d15fe042e29533afe0ce81f1
Riley Mack <riley.j.mack@gmail.com>
2023-04-20 22:33:01 -0400

    vercel express follow through

--- server/index.js
+++ server/index.js
@@ -1,20 +1,37 @@
-const { db } = require("./db");
-const PORT = process.env.PORT || 5000;
-const app = require("./app");
-const seed = require("../script/seed");
+const app = require("express")();
+const { v4 } = require("uuid");
 
-const init = async () => {
-  try {
-    if (process.env.SEED === "true") {
-      await seed();
-    } else {
-      await db.sync();
-    }
-    // start listening (and create a 'server' object representing our server)
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
-  } catch (ex) {
-    console.log(ex);
-  }
-};
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-init();
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
+
+// const { db } = require("./db");
+// const PORT = process.env.PORT || 5000;
+// const app = require("./app");
+// const seed = require("../script/seed");
+
+// const init = async () => {
+//   try {
+//     if (process.env.SEED === "true") {
+//       await seed();
+//     } else {
+//       await db.sync();
+//     }
+//     // start listening (and create a 'server' object representing our server)
+//     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+//   } catch (ex) {
+//     console.log(ex);
+//   }
+// };
+
+// init();
